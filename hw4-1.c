1ac97673abaf4b5672a8554b6c3d3208287e9af5
nb23506 <167288305+nb23506@users.noreply.github.com>
2024-10-23 17:08:49 +0900

    Update hw4-1.c

--- hw4-1.c
+++ hw4-1.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    int x, y, z, average;
-
- 
-    //printf("Enter three numbers: ");
-    scanf("%d %d %d", &x, &y, &z);
-
-    // Menghitung nilai rata-rataa
-    average = (x + y + z) / 3;
-
-    // Menampilkan hasil
-    printf("Average is %d\n", average);
-
-    return 0;
-}
